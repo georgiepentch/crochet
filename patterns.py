ae72b8a166de0b0e40530a54ee246c540c597104
George Pentchev <georgiepentch@gmail.com>
2024-04-16 17:57:53 -0400

    fixed sphere()

--- patterns.py
+++ patterns.py
@@ -44,7 +44,7 @@ def view(pattern=None, max_size=5):
                     total += int(i)
             if row_ind == current_row:
                 instructions[current_item] = hl(instructions[current_item])
-            print(f"{str(row_ind + 1)} ({red(total)}):\t" + ', '.join(instructions))
+            print(f"{str(row_ind + 1)} ({red(total)}):  " + ', '.join(instructions))
         cmd = input()
         if (current_row == len(array) - 1 and current_item == len(array[current_row]) - 1) or cmd == 'stop':
             break
@@ -66,8 +66,9 @@ def sphere(n):
     req.add_header('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, '
                                  'like Gecko) Chrome/103.0.0.0 Safari/537.36')
     raw = str(urlopen(req).read()).split("pre>")[1:][0]
-    raw = '\n'.join([i.split(':')[1] for i in raw.split('\\n')[2:-2]])
-    return raw
+    raw = [i.split(':')[1] for i in raw.split('\\n')[2:-2]]
+    initial = raw[0].count(',') + 1
+    return str(initial) + '\n' + '\n'.join(raw)
 
 
 # RUNNING SPHERE FOR NOW
